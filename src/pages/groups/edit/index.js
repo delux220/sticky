8a3140881ce6b985fd3c004f75bc0e7f9b8dd6d3
Dave Luke <dave.luke@gmail.com>
2019-04-28 20:18:09 -0400

    things

--- src/pages/groups/edit/index.js
+++ src/pages/groups/edit/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import cookie from 'react-cookies'
 import axios from 'axios'
+import { EditorState, convertToRaw } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg'
 import { Form, Select } from 'antd'
 
@@ -9,14 +10,15 @@ import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 
 const accessToken = cookie.load('access_token')
 const FormItem = Form.Item
-const { Option } = Select;
+const { Option } = Select
 
 axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`
 
 class Group extends React.Component {
   state = {
-    group: {},
-    categories: []
+    group: null,
+    categories: [],
+    editorState: EditorState.createEmpty(),
   }
 
   componentDidMount() {
@@ -25,13 +27,62 @@ class Group extends React.Component {
     const { id } = params
 
     axios.get(`/api/groups/${id}`).then(result => {
-      this.setState({ group: result.data.group, categories: result.data.categories });
+      const { group } = result.data
+      const cats = []
+      group.group_categories.forEach(cat => {
+        cats.push(cat.category_id)
+      })
+
+      console.log(cats)
+      group.groupCategories = cats
+
+      this.setState({ group, categories: result.data.categories })
     })
   }
 
-  render() {
-    const { group, categories } = this.state
+  save = () => {
+    const { match } = this.props
+    const { params } = match
+    const { id } = params
+    const { editorState, group } = this.state
+    const { groupCategories } = group
+    const price = group.price_per_day
 
+    axios
+      .post(`/api/groups/${id}`, {
+        price_per_day: price,
+        groupCategories,
+        description_html: convertToRaw(editorState.getCurrentContent()),
+      })
+      .then(result => {
+        console.log(result)
+      })
+  }
+
+  handleCategoryChange = value => {
+    const { group } = this.state
+    group.groupCategories = value
+
+    this.setState({ group })
+  }
+
+  handlePriceChange = e => {
+    const { group } = this.state
+    group.price_per_day = e.target.value
+    console.log(group)
+    this.setState({ group })
+  }
+
+  changeEditorState = editorState => {
+    this.setState({ editorState })
+  }
+
+  render() {
+    const { group, categories, editorState } = this.state
+
+    if (group == null) {
+      return <div />
+    }
 
     return (
       <div>
@@ -42,21 +93,54 @@ class Group extends React.Component {
               <div className="card-body">
                 <h3>{group.name}</h3>
                 <div className="form-group">
-                  <FormItem label="Description">
-                    <div style={{padding: '1rem', border:'1px solid #eee'}}>
-                      <Editor />
+                  <FormItem label="Group Description">
+                    <p className="text-muted">
+                      This description is displayed to interested sponsors, not group members. Help
+                      sponsors understand what this group is about.
+                    </p>
+                    <div style={{ padding: '1rem', border: '1px solid #eee' }}>
+                      <Editor
+                        editorState={editorState}
+                        onEditorStateChange={this.changeEditorState}
+                      />
                     </div>
-                  
                   </FormItem>
                 </div>
-                <Select mode="multiple">
-                  {categories.map((cat) => {
-                      return (<Option key={cat.id} value={cat.id}>{cat.name}</Option>);
-                  })}
-                </Select>
+                <FormItem label="Categories">
+                  <p className="text-muted">Help relevant sponsors find you</p>
+                  <Select
+                    mode="multiple"
+                    style={{ width: '100%' }}
+                    onChange={this.handleCategoryChange}
+                    value={group.groupCategories}
+                  >
+                    {categories.map(cat => {
+                      return (
+                        <Option key={cat.id} value={cat.id}>
+                          {cat.name}
+                        </Option>
+                      )
+                    })}
+                  </Select>
+                </FormItem>
+                <FormItem label="Price per day ($)">
+                  <p className="text-muted">
+                    How much do you want to charge per day for a sticky post in your group?
+                  </p>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="price"
+                    placeholder="0.00"
+                    onChange={this.handlePriceChange}
+                    value={group.price_per_day}
+                  />
+                </FormItem>
+                <button className="btn btn-primary" type="button" onClick={this.save}>
+                  Save
+                </button>
               </div>
             </div>
-            
           </div>
         </div>
       </div>
